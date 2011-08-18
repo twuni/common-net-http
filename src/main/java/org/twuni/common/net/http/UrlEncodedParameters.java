bf7d5794ed69bded176d1e9f3fe78d3f3d3af8a9
Leo Accend <github.aeonic@spamgourmet.com>
2011-08-18 13:09:53 -0700

    Adding support for reading and writing URL-encoded parameters.

---
+++ src/main/java/org/twuni/common/net/http/UrlEncodedParameters.java
@@ -0,0 +1,57 @@
+package org.twuni.common.net.http;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
+import org.twuni.common.MultiValueMap;
+
+public class UrlEncodedParameters extends MultiValueMap<String, String> {
+
+	private static final String UTF_8 = "UTF-8";
+	
+	public UrlEncodedParameters() {
+	}
+
+	public UrlEncodedParameters( String parameters ) {
+		for( String pair : parameters.split( "&" ) ) {
+			int index = pair.indexOf( '=' );
+			String key = decode( pair.substring( 0, index ) );
+			String value = decode( pair.substring( index + 1 ) );
+			add( key, value );
+		}
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder string = new StringBuilder();
+		for( String key : keySet() ) {
+			for( String value : getAll( key ) ) {
+				if( string.length() > 0 ) {
+					string.append( "&" );
+				}
+				string.append( encode( key ) );
+				string.append( "=" );
+				string.append( encode( value ) );
+			}
+		}
+		return string.toString();
+	}
+
+	private String decode( String encoded ) {
+		try {
+			return URLDecoder.decode( encoded, UTF_8 );
+		} catch( UnsupportedEncodingException exception ) {
+			return encoded;
+		}
+	}
+
+	private String encode( String decoded ) {
+		try {
+			return URLEncoder.encode( decoded, UTF_8 );
+		} catch( UnsupportedEncodingException exception ) {
+			return decoded;
+		}
+	}
+
+}
