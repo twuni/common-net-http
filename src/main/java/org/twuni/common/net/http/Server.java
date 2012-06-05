cacfc6508f5d9c3418d4b469759707b65626c88b
Leo Accend <github.aeonic@spamgourmet.com>
2012-06-04 22:34:19 -0700

    Adding convenience method to Server for a Hello World example.

--- src/main/java/org/twuni/common/net/http/Server.java
+++ src/main/java/org/twuni/common/net/http/Server.java
@@ -3,17 +3,18 @@ package org.twuni.common.net.http;
 import org.twuni.common.Filter;
 import org.twuni.common.net.http.responder.RequestMapping;
 import org.twuni.common.net.http.responder.Responder;
+import org.twuni.common.net.http.responder.StaticResponder;
 import org.twuni.common.net.http.response.Response;
 import org.twuni.common.net.http.response.filter.DateFilter;
 
 public class Server extends org.twuni.common.net.Server {
 
-	public Server( int port, Filter<Response> filter, Class<?>... annotatedClasses ) {
-		this( port, new RequestMapping( annotatedClasses ), filter );
-	}
-
 	public Server( int port, Class<?>... annotatedClasses ) {
 		this( port, new RequestMapping( annotatedClasses ) );
+	}
+
+	public Server( int port, Filter<Response> filter, Class<?>... annotatedClasses ) {
+		this( port, new RequestMapping( annotatedClasses ), filter );
 	}
 
 	public Server( int port, Responder responder ) {
@@ -22,6 +23,10 @@ public class Server extends org.twuni.common.net.Server {
 
 	public Server( int port, Responder responder, Filter<Response> filter ) {
 		super( port, new WorkerFactory( responder, filter ) );
+	}
+
+	public Server( int port, String response ) {
+		this( port, new StaticResponder( response ) );
 	}
 
 }
